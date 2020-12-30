124bcb8f7172e6170e98f0d44c64a46485543ee3
ItsNotkungZ <69802296+ItsNotkungZ@users.noreply.github.com>
2020-12-31 02:51:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new discord.Client();
 const ytdl = require('ytdl-core');
 
 client.on('ready', async () => {
-  client.user.setActivity(STATUS + " 😎")
+  client.user.setActivity(process.env.STATUS + " 🌍")
   let channel = client.channels.cache.get(process.env.CHANNEL) || await client.channels.fetch(process.env.CHANNEL)
 
   if(!channel) return;
